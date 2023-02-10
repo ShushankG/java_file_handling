518cebf812c4073639da856041a90325524aa316
ShushankG <98677703+ShushankG@users.noreply.github.com>
2023-02-10 22:51:44 +0530

    Add files via upload

---
+++ Java file handling/PrintStream/Rusk.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Rusk{
+
+public static void main(String args[])throws Exception{
+
+PrintStream ps = new PrintStream("ab.txt");
+ps.print("Hi i am Shushank gautam how are you");
+ps.close();
+
+
+
+}
+
+
+}
